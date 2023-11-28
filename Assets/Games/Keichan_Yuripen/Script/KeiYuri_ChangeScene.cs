194d41a2645f82559c95103f5cfa503f45614609
keichange <keichange@users.noreply.github.com>
2023-11-29 01:45:51 +0900

    アイコンクリックでScene遷移できるようになった。
    https://www.hanachiru-blog.com/entry/2019/02/26/203740

--- Assets/Games/Keichan_Yuripen/Script/KeiYuri_ChangeScene.cs
+++ Assets/Games/Keichan_Yuripen/Script/KeiYuri_ChangeScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameSelectButton : MonoBehaviour
+public class KeiYuri_ChangeScene : MonoBehaviour
 {
     public string SceneName;
     // Start is called before the first frame update
