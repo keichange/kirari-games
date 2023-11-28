1151cdc8afbd5bf14c0b206f033dbfe68d9608a7
keichange <keichange@users.noreply.github.com>
2023-11-29 01:11:19 +0900

    メニューアイコン上側すべて入れた、Scriptable Objectで情報の管理
    メニューアイコンときらりっちの画像を入れた。
    お金、満腹度、機嫌の情報をScriptable Objectを使うことによってシーンをまたがって使うことができるようになった。

---
+++ Assets/Games/Keichan_Yuripen/Script/keiYuri_Data.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Kiraritti_Data")]
+public class KeiYuri_Data : ScriptableObject
+{
+    public int money;
+    public int satietyLevel;
+    public int mood;
+}
