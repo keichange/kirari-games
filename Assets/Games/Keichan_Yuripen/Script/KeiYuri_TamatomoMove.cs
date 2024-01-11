fbb1f100a7233562df63a4c5d075be364ec2a062
keichange <104511370+keichange@users.noreply.github.com>
2024-01-11 10:00:44 +0900

    レストランで食べるときにお金が減るようにした

---
+++ Assets/Games/Keichan_Yuripen/Script/KeiYuri_TamatomoMove.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeiYuri_TamatomoMove : MonoBehaviour
+{
+    [SerializeField]
+    private KeiYuri_TamatomoManager tm;
+    private KeiYuri_TamatomoData currentTamatomo;
+    private void OnEnable()
+    {
+        currentTamatomo = tm.currentTamatomo;
+    }
+}
