0e18bd5003a742fbaa15cae7a43f1922712530ed
keichange <104511370+keichange@users.noreply.github.com>
2024-01-15 21:31:41 +0900

    たまともを帰らせる機能を作ろうとしている

--- Assets/Games/Keichan_Yuripen/Script/Main/KeiYuri_TamatomoMove.cs
+++ Assets/Games/Keichan_Yuripen/Script/Main/KeiYuri_TamatomoMove.cs
@@ -19,6 +19,10 @@ public class KeiYuri_TamatomoMove : MonoBehaviour
         {
             sr.sprite = currentTamatomo.idle;
             gameObject.SetActive(true);
+            if(currentTamatomo.IsLeave())
+            {
+                StartCoroutine(LeaveAnimation());
+            }
         }
         else gameObject.SetActive(false);
         
@@ -45,5 +49,24 @@ public class KeiYuri_TamatomoMove : MonoBehaviour
         sr.sprite = currentTamatomo.yorokobiSprites[1];
         yield return new WaitForSeconds(waitTime);
         sr.sprite = currentTamatomo.yorokobiSprites[0];
+    }
+
+    IEnumerator LeaveAnimation()
+    {
+        sr.sprite = currentTamatomo.yorokobiSprites[0];
+        yield return new WaitForSeconds(waitTime);
+        sr.sprite = currentTamatomo.yorokobiSprites[1];
+        yield return new WaitForSeconds(waitTime);
+        sr.sprite = currentTamatomo.yorokobiSprites[0];
+        transform.position = new Vector2(2.5f, -0.6f);
+        yield return new WaitForSeconds(waitTime);
+        sr.sprite = currentTamatomo.yorokobiSprites[1];
+        transform.position = new Vector2(2.5f, 1.5f);
+        yield return new WaitForSeconds(waitTime);
+        sr.sprite = currentTamatomo.yorokobiSprites[0];
+        transform.position = new Vector2(2.5f, 3.5f);
+        yield return new WaitForSeconds(waitTime);
+
+        currentTamatomo = null;
     }
 }
